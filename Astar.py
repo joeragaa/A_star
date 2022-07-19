58df8ae0b9a309f1f1f95e24c41cd48dcb300da1
joeragaa <48696985+joeragaa@users.noreply.github.com>
2022-07-19 17:20:25 +0200

    visuals done

--- Astar.py
+++ Astar.py
@@ -14,6 +14,7 @@ def distance(x1, y1, x2, y2):
     else:
         return 14
 
+
 class node:
     def __init__(self, a=-1, b=-1):
         self.x = a
@@ -34,7 +35,8 @@ class node:
         return (self.x == n.x) and (self.y == n.y)
 
     def calHcost(self, dest):
-        self.Hcost = math.sqrt((self.x - dest.x) ** 2 + (self.y - dest.y) ** 2)*10
+        self.Hcost = math.sqrt((self.x - dest.x) ** 2 +
+                               (self.y - dest.y) ** 2)*10
 
     # return a list of the nodes neighboring this node
     def get_neighbors(self, dest):
@@ -49,26 +51,31 @@ class node:
                 neighbor = node(i, j)
                 neighbor.parentx = self.x
                 neighbor.parenty = self.y
-                neighbor.Gcost = self.Gcost + distance(self.x, self.y, dest.x, dest.y)
+                neighbor.Gcost = self.Gcost + \
+                    distance(self.x, self.y, dest.x, dest.y)
                 neighbor.calHcost(dest)
                 neighbor.Fcost = neighbor.Gcost + neighbor.Hcost
                 neighbors.append(neighbor)
         return neighbors
 
     def print(self):
-        print(self.x,self.y)
+        print(self.x, self.y)
+
 
 vis_list = []
-def trace(list,n,source):
+
+
+def trace(list, n, source):
     if n == source:
         n.print()
         vis_list.append(n)
-        return 
+        return
     else:
         n.print()
         vis_list.append(n)
-        n = [i for i in list if i == node(n.parentx,n.parenty)].pop()
-        trace(list,n,source)
+        n = [i for i in list if i == node(n.parentx, n.parenty)].pop()
+        trace(list, n, source)
+
 
 def Asearch(grid, src, dest):
     open = list()
@@ -85,7 +92,7 @@ def Asearch(grid, src, dest):
             if candidate == dest:
                 print("destination found")
                 closed.append(current_node)
-                trace(closed,candidate,src)
+                trace(closed, candidate, src)
                 return
 
             if candidate in closed and closed[closed.index(candidate)] < candidate:
@@ -101,40 +108,43 @@ def Asearch(grid, src, dest):
 
     print("destination not found")
 
-    
+
 def display(arr):
     figure = plt.figure()
     axes = figure.add_subplot()
     axes = axes.matshow(arr)
-    cb_labels = ['obstacle','open','goal','destination','path']
+    cb_labels = ['obstacle', 'open', 'goal', 'destination', 'path']
     cb = figure.colorbar(axes)
     cb.ax.get_yaxis().set_ticks(list(range(len(cb_labels))))
     cb.ax.set_yticklabels(cb_labels)
     plt.show()
 
-#get a valid input point from the user
+# get a valid input point from the user
+
+
 def get_input(isPoint=False):
     user_input = input("format: x y >> ")
     if user_input is None:
         raise ValueError("invalid input")
-    
-    if user_input=='q':
+
+    if user_input == 'q':
         raise Exception
-    
-    x,y=tuple(int(a) for a in user_input.split())
+
+    x, y = tuple(int(a) for a in user_input.split())
     if x is None or y is None:
         raise ValueError("invalid input")
-         
-    if (isPoint==True) and (x<0 or y<0 or x>gridX or y>gridY):
+
+    if (isPoint == True) and (x < 0 or y < 0 or x > gridX or y > gridY):
         raise ValueError("invalid input")
-        
+
     else:
-        return x,y
+        return x, y
+
 
 def main():
     print("enter map dimensions")
     try:
-        gridX,gridY = get_input()
+        gridX, gridY = get_input()
     except:
         print("map dimensions must be positive integers")
         return
@@ -143,7 +153,7 @@ def main():
     print("enter the obstacles on the map or hit q to exit")
     try:
         while(1):
-            i,j = get_input(isPoint=True)
+            i, j = get_input(isPoint=True)
             grid[i][j] = 0
     except ValueError:
         print("obstacles must be within 0 and specified map dimension")
@@ -153,7 +163,7 @@ def main():
 
     print("enter source node")
     try:
-        i,j = get_input(isPoint=True)
+        i, j = get_input(isPoint=True)
         src = node(i, j)
         src.Gcost = 0
     except:
@@ -161,22 +171,22 @@ def main():
 
     print("enter destination node")
     try:
-        i,j = get_input(isPoint=True)
+        i, j = get_input(isPoint=True)
         destination = node(i, j)
     except:
         return
 
-    #search 
-    t= time.time()
+    # search
+    t = time.time()
     Asearch(grid, src, destination)
     print(f"time to search {int((time.time()-t)*1000)} milliseconds")
 
-    #visulization steps
-    #an arbitrary value is given to the source, destination and path nodes for visualization purposes
+    # visulization steps
+    # an arbitrary value is given to the source, destination and path nodes for visualization purposes
     for t in vis_list:
-        grid[t.x][t.y]=4
-    grid[src.x][src.y]=2
-    grid[destination.x][destination.y]=3
+        grid[t.x][t.y] = 4
+    grid[src.x][src.y] = 2
+    grid[destination.x][destination.y] = 3
     display(grid)
 
 
